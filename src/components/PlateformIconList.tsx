4915f132e24d5990ca9a8371dc61b13bd933ec04
Irnam Soimansoib <irnam.sa@gmail.com>
2024-06-04 12:49:04 +0200

    Add keys to plateform icon components

--- src/components/PlateformIconList.tsx
+++ src/components/PlateformIconList.tsx
@@ -33,7 +33,11 @@ const PlateformIconList = ({ platforms }: Props) => {
   return (
     <HStack marginY={2}>
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color="gray.500" />
+        <Icon
+          key={platform.slug}
+          as={iconMap[platform.slug]}
+          color="gray.500"
+        />
       ))}
     </HStack>
   );
